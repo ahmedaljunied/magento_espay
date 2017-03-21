7901f5c30e82c2831913de013de972e501e38307
Antoni F. Setiawan <antoni.fs@gmail.com>
2017-03-21 15:09:47 +0700

    add cc

---
+++ app/code/local/Plus/Espaypaymentmethod/Model/paymentmethod_mandiri.php
@@ -0,0 +1,5 @@
+<?php
+// app/code/local/Envato/Custompaymentmethod/Model/Paymentmethod.php
+class Plus_Espaypaymentmethod_Model_Paymentmethod_cc extends Plus_Espaypaymentmethod_Model_Abstract {
+  protected $_code  = 'espaypaymentmethod_mandiri';
+}
