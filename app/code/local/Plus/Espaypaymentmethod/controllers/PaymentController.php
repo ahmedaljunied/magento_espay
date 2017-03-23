a251189e251b58ae4313e9a5e4ec5e9f854fc3af
Antoni F. Setiawan <antoni.fs@gmail.com>
2017-03-23 13:38:54 +0700

    updated cancelation

--- app/code/local/Plus/Espaypaymentmethod/controllers/PaymentController.php
+++ app/code/local/Plus/Espaypaymentmethod/controllers/PaymentController.php
@@ -268,10 +268,46 @@ class Plus_Espaypaymentmethod_PaymentController extends Mage_Core_Controller_Fro
             } else {
                 Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/success', array('_secure' => false));
                 Mage::log(print_r($order, 1), null, 'espay_responsesuccess.log');
+
+                // // Start
+                // $url = "http://vendor.tinkerlust.com/api_paymentconfirm.php?payment_sd=yes&name=". urlencode($name) . "&email=". urlencode($email) . "&order_no=". urlencode($order_no)."&total=". urlencode($total) ."&transfer_no=" . urlencode($transfer_no) . "&transfer_bank=".urlencode($transfer_bank);
+                // Mage::log(print_r($url, 1), null, 'espay_querystring.log');
+
+                // $curl = curl_init();
+                // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                // curl_setopt($curl, CURLOPT_URL, $url);
+                // curl_setopt($curl, CURLOPT_USERAGENT, 1);
+                // curl_setopt($curl, CURLOPT_POST, 1);
+                // $resp = curl_exec($curl);
+                // curl_close($curl);        
+                // // end
             }
         } else {
             Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/failure', array('_secure' => false));
-            Mage::log(print_r($order, 1), null, 'espay_responsefailed.log');
+            if ($order->canCancel()) {
+                try {
+                    $order->cancel();
+                    $order->getStatusHistoryCollection(true);
+                    $order->save();
+                    Mage::log("Order canceled", null, 'espay_responsefailed.log');
+
+                    // // Start
+                    // $url = "http://vendor.tinkerlust.com/api_paymentconfirm.php?payment_sd=yes&name=". urlencode($name) . "&email=". urlencode($email) . "&order_no=". urlencode($order_no)."&total=". urlencode($total) ."&transfer_no=" . urlencode($transfer_no) . "&transfer_bank=".urlencode($transfer_bank);
+                    // Mage::log(print_r($url, 1), null, 'espay_querystring.log');
+
+                    // $curl = curl_init();
+                    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                    // curl_setopt($curl, CURLOPT_URL, $url);
+                    // curl_setopt($curl, CURLOPT_USERAGENT, 1);
+                    // curl_setopt($curl, CURLOPT_POST, 1);
+                    // $resp = curl_exec($curl);
+                    // curl_close($curl);        
+                    // // end
+                    
+                } catch (Exception $e) {
+                    Mage::logException($e);
+                }
+            }
         }
     }
 
